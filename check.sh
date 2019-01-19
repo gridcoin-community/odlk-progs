88ebea767ddc4ef7b8407a5a978b2f3bb1b20288
Tomáš Brada <tomasbrod@azet.sk>
2019-01-19 19:52:36 +0100

    Add test cases for kanonizator_lk and ortogon.

--- check.sh
+++ check.sh
@@ -17,4 +17,19 @@ echo Check kanonizator_dlk
 ../kanonizator_dlk.exe ../kanonizator_dlk/input.txt kan_out.txt >/dev/null
 cmp kan_out.txt ../kanonizator_dlk/output.txt
 
+echo Check kanonizator_lk
+../kanonizator_lk.exe /m ../kanonizator_lk/input.txt kanlk_out.txt >/dev/null
+cmp kanlk_out.txt ../kanonizator_lk/output.txt
+
+echo Check ortogon
+cp ../psevdoass/mar_abkm_4.3.7.2.1.9.5.6.txt out.txt
+../ortogon.exe out.txt >/dev/null
+cmp mout.txt ../ortogon/mout.txt
+
+if [ -e ../psevdoass.exe ]; then
+echo Check psevdoass
+../psevdoass.exe -abk pskan.txt 4 3 7 2 1 9 5 6 >/dev/null
+cmp pskan.txt ../psevdoass/mar_abk_4.3.7.2.1.9.5.6.txt
+else echo "Skip psevdoass (not built)"; fi
+
 echo ok
