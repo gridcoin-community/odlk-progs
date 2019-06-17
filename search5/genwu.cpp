eab5505c8996d8ddfc0c0281c7375912789ff9f6
Tomáš Brada <tomasbrod@azet.sk>
2019-06-17 20:27:41 +0200

    add genwu

--- search5/genwu.cpp
+++ search5/genwu.cpp
@@ -1,1 +1,109 @@
 /* Generate workunits from segments that are idle */
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <bitset>
+#include <ctime>
+#include <unistd.h>
+#include <cstdlib>
+#include <sys/stat.h>
+
+#include "odlkcommon/common.h"
+#include "odlkcommon/namechdlk10.h"
+#include "family_mar/prov_blk_trans.h"
+
+#include "boinc_api.h"
+#include "Stream.cpp"
+
+#include "config.h"
+#include "backend_lib.h"
+#include "error_numbers.h"
+#include "sched_config.h"
+#include "sched_util.h"
+
+#include "odlkcommon/namechdlk10.cpp"
+#include "odlkcommon/kvio.cpp"
+
+#include "wio.cpp"
+#include "gener.cpp"
+
+void initz() {
+	int retval = config.parse_file();
+	if (retval) {
+			log_messages.printf(MSG_CRITICAL,
+					"Can't parse config.xml: %s\n", boincerror(retval)
+			);
+			exit(1);
+	}
+
+	retval = boinc_db.open(
+			config.db_name, config.db_host, config.db_user, config.db_passwd
+	);
+	if (retval) {
+			log_messages.printf(MSG_CRITICAL,
+					"boinc_db.open failed: %s\n", boincerror(retval)
+			);
+			exit(1);
+	}
+}
+
+class DB_GENITEM : public DB_BASE {
+public:
+	//needed: rule, minl, next
+	DB_ID_TYPE id;
+	int rule;
+	int minl;
+	NamerCHDLK10::NameStr next;
+public:
+    DB_GENITEM(DB_CONN* p=0) : DB_BASE("tot_segment", p?p:&boinc_db) {}
+    void db_parse(MYSQL_ROW &r) {
+			id = atoi(r[0]);
+			rule = atoi(r[1]);
+			minl = atoi(r[4]);
+			std::copy(r[5],r[5]+next.size(),next.begin());
+		}
+};
+
+int main(int argc, char** argv) {
+	bool f_write;
+	long gen_limit;
+	char *check1;
+	if(argc!=3) {
+			cerr<<"Expect 2 command line argument: f_write limit"<<endl;
+			exit(2);
+	}
+	f_write = (argv[1][0]=='y');
+	gen_limit = strtol(argv[2],&check1,10);
+	if((argv[1][0]!='n' && !f_write) || *check1) {
+			cerr<<"Invalid argument format"<<endl;
+			exit(2);
+	}
+	cerr<<"f_write="<<f_write<<" limit="<<gen_limit<<endl;
+	//connect db if requested
+	initz();
+	if(boinc_db.start_transaction())
+		exit(4);
+	//do generate actually
+	NamerCHDLK10::init();
+	while(1) {
+		std::stringstream qr;
+		qr<<"where cur_wu is null and next is not null and enabled limit "
+		<<gen_limit<<";";
+		DB_GENITEM item;
+		int retval= item.enumerate(qr.str().c_str());
+		if(retval) {
+			if (retval != ERR_DB_NOT_FOUND) {
+				cerr<<"db error"<<endl;
+				exit(4);
+			}
+			break;
+		}
+		(cout<<"Itm"<<item.id<<" r"<<item.rule<<" m"<<item.minl<<" next").write(item.next.data(),item.next.size())<<endl;
+	}
+
+	//select * from tot_segment where cur_wu is null and next is not null and enabled;
+	//create input file
+	//create_work()
+	return 1;
+}
